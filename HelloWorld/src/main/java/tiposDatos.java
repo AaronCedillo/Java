964758b02c8749c2aa0189ec585c049f1ed9d754
AaronCedillo <adrian_sonny55@hotmail.com>
2019-09-13 12:57:53 -0500

    Datos char con Unicode

--- HelloWorld/src/main/java/tiposDatos.java
+++ HelloWorld/src/main/java/tiposDatos.java
@@ -24,13 +24,23 @@ public class tiposDatos {
         System.out.println("Bits en el tipo Float: " + Float.SIZE);
         System.out.println("Bytes en el tipo Float: " + Float.BYTES);
         System.out.println("Valor minimo en el tipo Float: " + Float.MIN_VALUE);
-        System.out.println("Valor maximo en el tipo float: " + Float.MAX_VALUE);
+        System.out.println("Valor maximo en el tipo float: " + Float.MAX_VALUE + "\n");
         
         var doubleVar = 1000.10D;
         System.out.println("Valor double: " + doubleVar);
         System.out.println("Bits en el tipo Float: " + Double.SIZE);
         System.out.println("Bytes en el tipo Float: " + Double.BYTES);
         System.out.println("Valor minimo en el tipo Float: " + Double.MIN_VALUE);
-        System.out.println("Valor maximo en el tipo float: " + Double.MAX_VALUE);
+        System.out.println("Valor maximo en el tipo float: " + Double.MAX_VALUE + "\n");
+        
+        /// **** Valores tipo char ****///
+        char charValue = 'a';
+        char charUnicode = '\u0021';   /// Es posible utilizar 'var' y encontrando '\ u' se tomara como unicode
+        System.out.println("Valor char: " + charValue);
+        System.out.println("Valor char en Unicode: " + charUnicode);
+        System.out.println("Bits en el tipo Char: " + Character.SIZE);
+        System.out.println("Bytes en el tipo Char: " + Character.BYTES);
+        System.out.println("Valor minimo en el tipo Char: " + Character.MIN_VALUE);
+        System.out.println("Valor maximo en el tipo Char: " + Character.MAX_VALUE);
     }
 }
