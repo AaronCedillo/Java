6247ee0ca63eda467ad01350c4f40c3a3514ad84
AaronCedillo <adrian_sonny55@hotmail.com>
2019-09-12 21:20:16 -0500

    Variable de tipo String

---
+++ HelloWorld/src/main/java/StringData.java
@@ -0,0 +1,12 @@
+/*
+ * @author AdrianCedillo
+ */
+public class StringData {
+    public static void main(String[] args) {
+        String saludo = "Hola mundo";
+        System.out.println(saludo);
+        
+        var despedirse = "Adios";
+        System.out.println(despedirse);
+    }
+}
