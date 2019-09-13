7f39b6dd8589d24467a14f7a038157c15b1f027c
AaronCedillo <adrian_sonny55@hotmail.com>
2019-09-13 11:43:01 -0500

    Manejo de sistemas numericos

---
+++ HelloWorld/src/main/java/tiposDatos.java
@@ -0,0 +1,18 @@
+/*
+ * @author AdrianCedillo
+ */
+public class tiposDatos {
+    public static void main(String[] args) {
+        int numeroDecimal = 10;
+        System.out.println("numeroDecimal: " + numeroDecimal);
+        
+        int numeroOctal = 012;
+        System.out.println("NumeroOctal: " + numeroOctal); //Imprime el valor en decimal
+        
+        int numeroHexadecimal = 0xA;
+        System.out.println("NumeroHexadecimal: " + numeroHexadecimal);
+        
+        int numeroBinario = 0b1010;
+        System.out.println("NumeroBinario: " + numeroBinario);
+    }
+}
