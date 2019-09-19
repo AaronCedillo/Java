2e48b69b7263028445355e7b62f711ef58eb8727
AaronCedillo <adrian_sonny55@hotmail.com>
2019-09-19 17:37:09 -0500

    Sentencia switch en Java, nada nuevo aun....

--- HelloWorld/src/main/java/sentenciaSwitch.java
+++ HelloWorld/src/main/java/sentenciaSwitch.java
@@ -1,13 +1,31 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+import java.util.Scanner;
+/**
+ * @author AaronCedillo
  */
 
-/**
- *
- * @author Kanan
- */
 public class sentenciaSwitch {
-    
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+                
+        System.out.println("Ingrese un numero: ");
+        String texto;
+        var numeroInput = scanner.nextLine();
+        int numero = Integer.parseInt(numeroInput);
+        
+        switch(numero) {
+            case 1:
+                texto = "Numero uno";
+                break;
+            case 2:
+                texto = "Numero dos";
+                break;
+            case 3:
+                texto = "Numero tres";
+                break;
+            default:
+                texto = "Numero desconocido";
+                break;
+        }
+        System.out.println("texto = " + texto);
+    }
 }
