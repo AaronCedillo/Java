40078ba41f5489fbefd33e559aa1df890d7d1d13
AaronCedillo <adrian_sonny55@hotmail.com>
2019-09-20 09:01:07 -0500

    Ciclo for en Java

---
+++ HelloWorld/src/main/java/cicloFor.java
@@ -0,0 +1,10 @@
+/*
+ * @author AaronCedillo
+ */
+public class cicloFor {
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i++) {
+            System.out.println("i = " + i);
+        }
+    }
+}
