9a058c460704f6e7c9a1b18a4859351684a5e8d2
AaronCedillo <adrian_sonny55@hotmail.com>
2019-09-21 18:29:15 -0500

    Metodos en Java

--- ProyectoAritmetica/src/main/java/Aritmetica.java
+++ ProyectoAritmetica/src/main/java/Aritmetica.java
@@ -1,8 +1,8 @@
 /*
  * @author aaroncedillo
  */
-public class Aritmetica {
-    //Creamos el metodo
+public class Aritmetica {      
+    // Creamos el metodo
     public int sumar(int a, int b){
         int resultado = a + b;
         return resultado;
