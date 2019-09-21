b394f569eb95bca8e3219275776c80299e3ffb61
AaronCedillo <adrian_sonny55@hotmail.com>
2019-09-20 22:43:40 -0500

    Empezando a crear objetos y dandoles atributos

---
+++ CreacionClasesObjetos/src/main/java/PersonaPrueba.java
@@ -0,0 +1,22 @@
+/*
+ * @author Kanan
+ */
+public class PersonaPrueba {
+    public static void main(String[] args) {
+        //Creamos un objeto
+        Persona persona1 = new Persona();
+        
+        //Llamamos al metodo del objeto creado
+        System.out.println("valores por default del objeto perona");
+        persona1.desplegarNombre();
+        
+        System.out.println("Agregamos datos a los atributos del objeto" + "\n");
+        //Modificar valores del objeto
+        persona1.nombre = "Adrian";
+        persona1.apellidoPaterno = "Cedillo";
+        persona1.apellidoMaterno = "Huerta";
+        
+        //Volvemos a llamar al metodo
+        persona1.desplegarNombre();
+    }   
+}
