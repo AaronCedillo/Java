d1f835b727819cf902efc50269221b6ca576a958
AaronCedillo <adrian_sonny55@hotmail.com>
2019-09-20 22:18:06 -0500

    Creacion de objetos y clases

---
+++ CreacionClasesObjetos/src/main/java/Persona.java
@@ -0,0 +1,16 @@
+/*
+ * @author AaronCedillo
+ */
+public class Persona {
+    //Definimos atributos
+    String nombre;
+    String apellidoPaterno;
+    String apellidoMaterno;
+    
+    //Definimos metodos
+    public void desplegarNombre() {
+        System.out.println("nombre = " + nombre);
+        System.out.println("apellidoPaterno = " + apellidoPaterno);
+        System.out.println("apellidoMaterno = " + apellidoMaterno);
+    }
+}
