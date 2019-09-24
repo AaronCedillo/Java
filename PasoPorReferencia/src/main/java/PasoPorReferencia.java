984a582a03b1117ca7e12cb5a33010a472d46f1b
AaronCedillo <adrian_sonny55@hotmail.com>
2019-09-24 11:37:47 -0500

    Ejercicio de PasoPorReferencia

--- PasoPorReferencia/src/main/java/PasoPorReferencia.java
+++ PasoPorReferencia/src/main/java/PasoPorReferencia.java
@@ -1,6 +1,13 @@
 /*
  * @author AaronCedillo
  */
+// Primero creamos una variable "p" de nuestro objeto Persona que estamos creando,
+// con esta variable tendremos acceso a los metodos de la clase a la cual pertenece nuestro objeto,
+// ya creada esta variable accederemos al metodo cambiarNombre() donde asignaremos un nombre,
+// este sera el primer valor que contiene nuestra referencia de memoria del objeto,
+// luego llamamos al metodo modificarPersona, aqui se pasar por referencia el valor que contiene
+// la direccion de memoria, por tanto se modifica la referencia de memoria, no solo la variable.
+
 public class PasoPorReferencia {
     public static void main(String[] args) {
         Persona p = new Persona();
