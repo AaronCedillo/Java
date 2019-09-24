7bd444c040b567665268858098b677c9644a9973
AaronCedillo <adrian_sonny55@hotmail.com>
2019-09-24 09:54:32 -0500

    Constructores, metodos, palabra reservada This

--- ConstructoresAritmetica/src/main/java/Aritmetica.java
+++ ConstructoresAritmetica/src/main/java/Aritmetica.java
@@ -6,21 +6,32 @@ public class Aritmetica {
     // Definimos atributos de la clase
     int a;
     int b;
-
+    
     // Agregamos un constructor vacio de la clase
     Aritmetica() {
 
     }
 
     // Constructor con dos argumentos
-    Aritmetica(int arg1, int arg2) {
-        a = arg1;
-        b = arg2;
+    Aritmetica(int a, int b) {
+        this.a = a;
+        this.b = b;
     }
     
     // Este metodo sumar toma los atributos de la clase
     int sumar(){  // Los metodos en Java corresponden a una accion
         return (a + b);
     }
-
+    
+    int restar() {
+        return (a - b);
+    }
+    
+    int multiplicar() {
+        return (a * b);
+    }
+    
+    int dividir() {
+        return (a / b);
+    }
 }
--- ConstructoresAritmetica/src/main/java/PruebaAritmetica.java
+++ ConstructoresAritmetica/src/main/java/PruebaAritmetica.java
@@ -3,6 +3,7 @@
  */
 public class PruebaAritmetica {
     public static void main(String[] args) {
+        /*
         // Creamos el objeto
         Aritmetica objeto1 = new Aritmetica();  // Se manda a llamar el constructor vacio de la clase, este objeto 
                                                 // se crea en una referencia de memoria
@@ -17,5 +18,27 @@ public class PruebaAritmetica {
         
         Aritmetica objeto2 = new Aritmetica(5, 3);
         System.out.println("objeto2 = " + objeto2.sumar());
+        */
+    ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+        //Ejercicio: Alcance de variables en Java
+        
+        //Definimos variables locales
+        int operandoA = 6;
+        int operandoB = 2;
+        
+        // Creamos un objeto de la clase Aritmetica enviando argumentos
+        Aritmetica objeto1 = new Aritmetica(operandoA, operandoB); 
+        
+        //Imprimimos el resultado del metodo sumar()
+        System.out.println("suma = " + objeto1.sumar());
+        
+        // Resultado del metodo restar()
+        System.out.println("resta = " + objeto1.restar());
+        
+        // Resultado del metodo multiplicar()
+        System.out.println("multiplicacion = " + objeto1.multiplicar());
+        
+        // Resultado del metodo dividir()
+        System.out.println("division = " + objeto1.dividir());
     }
 }
