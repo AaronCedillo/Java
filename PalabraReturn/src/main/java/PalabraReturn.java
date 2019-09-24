900363d8f176986f2c41d0b06b40973405d084e6
AaronCedillo <adrian_sonny55@hotmail.com>
2019-09-24 12:14:16 -0500

    Uso de la palabra return

---
+++ PalabraReturn/src/main/java/PalabraReturn.java
@@ -0,0 +1,13 @@
+/*
+ * @author AaronCedillo
+ */
+public class PalabraReturn {
+
+    public static void main(String[] args) {
+        var resultado = sumar(3, 6);
+        System.out.println("resultado = " + resultado);
+    }
+    public static int sumar(int a, int b) {
+        return (a + b);
+    }
+}
